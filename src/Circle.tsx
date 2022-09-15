670b268f302aafce7b56b6f91ba5dea41e575dca
k-sang-soo <67569730+k-sang-soo@users.noreply.github.com>
2022-09-15 16:54:28 +0900

    Feat: CRYPTO TRACKER project

---
+++ src/Circle.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+interface CircleProps {
+    bgColor: string;
+    borderColor?: string;
+    text?: string;
+}
+
+const Container = styled.div<CircleProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${props => props.bgColor};
+  border: 1px solid ${props => props.borderColor};
+`;
+
+
+function Circle({bgColor, borderColor, text = "default text"}: CircleProps) {
+    return (
+        <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+            {text}
+        </Container>
+    );
+}
+
+export default Circle;
