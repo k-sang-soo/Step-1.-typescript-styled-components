d8621e431ecd348c40401359d13922b5c80d0b3b
k-sang-soo <wsckm1@google.com>
2022-09-20 22:34:59 +0900

    price page 제작 중...

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,60 +1,60 @@
-import {Link, Outlet, useLocation, useMatch, useOutletContext, useParams} from "react-router-dom";
-import styled from "styled-components";
-import {useEffect, useState} from "react";
-import {useQuery} from "@tanstack/react-query";
-import {fetchCoinInfo, fetchCoinTickers} from "../api";
-import {Helmet} from 'react-helmet';
+import { Link, Outlet, useLocation, useMatch, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { fetchCoinInfo, fetchCoinTickers } from '../api';
+import { Helmet } from 'react-helmet';
 
 const Container = styled.div`
-  max-width: 480px;
-  margin: 0 auto;
-  padding: 0 20px;
+    max-width: 480px;
+    margin: 0 auto;
+    padding: 0 20px;
 `;
 
 const Header = styled.header`
-  text-align: center;
+    text-align: center;
 `;
 
 const Title = styled.h1`
-  font-size: 48px;
-  color: ${props => props.theme.accentColor};
+    font-size: 48px;
+    color: ${(props) => props.theme.accentColor};
 `;
 
 const BackPageBtn = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin: 10px auto;
-  a {
-    display: block;
-    border: 1px solid ${props => props.theme.textColor};
-    border-radius: 50%;
-    width: 32px;
-    height: 32px;
-    font-size: 20px;
-  }
-`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px auto;
+    a {
+        display: block;
+        border: 1px solid ${(props) => props.theme.textColor};
+        border-radius: 50%;
+        width: 32px;
+        height: 32px;
+        font-size: 20px;
+    }
+`;
 
 const Overview = styled.div`
-  display: flex;
-  justify-content: space-between;
-  background-color: rgba(0, 0, 0, 0.5);
-  padding: 10px 20px;
-  border-radius: 10px;
+    display: flex;
+    justify-content: space-between;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 10px 20px;
+    border-radius: 10px;
 `;
 const OverviewItem = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  span:first-child {
-    font-size: 10px;
-    font-weight: 400;
-    text-transform: uppercase;
-    margin-bottom: 5px;
-  }
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    span:first-child {
+        font-size: 10px;
+        font-weight: 400;
+        text-transform: uppercase;
+        margin-bottom: 5px;
+    }
 `;
 const Description = styled.p`
-  margin: 20px 0px;
+    margin: 20px 0px;
 `;
 
 const Loader = styled.div`
@@ -62,30 +62,30 @@ const Loader = styled.div`
 `;
 
 const Tabs = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  margin: 25px 0px;
-  gap: 10px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    margin: 25px 0px;
+    gap: 10px;
 `;
 
 const Tab = styled.span<{ isActive: boolean }>`
-  text-align: center;
-  text-transform: uppercase;
-  font-size: 12px;
-  font-weight: 400;
-  background-color: rgba(0, 0, 0, 0.5);
-  border-radius: 10px;
-  color: ${props => props.isActive ? props.theme.accentColor : props.theme.textColor};
-  a {
-    display: block;
-    padding: 7px 0px;
-  }
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 12px;
+    font-weight: 400;
+    background-color: rgba(0, 0, 0, 0.5);
+    border-radius: 10px;
+    color: ${(props) => (props.isActive ? props.theme.accentColor : props.theme.textColor)};
+    a {
+        display: block;
+        padding: 7px 0px;
+    }
 `;
 
 interface RouteState {
     state: {
         name: string;
-    }
+    };
 }
 
 interface InfoData {
@@ -109,7 +109,7 @@ interface InfoData {
     last_data_at: string;
 }
 
-interface PriceData {
+export interface PriceData {
     id: string;
     name: string;
     symbol: string;
@@ -139,17 +139,17 @@ interface PriceData {
             price: number;
             volume_24h: number;
             volume_24h_change_24h: number;
-        }
-    }
+        };
+    };
 }
 
 function Coin() {
-    const {coinId} = useParams();
-    const {state} = useLocation() as RouteState;
-    const priceMatch = useMatch("/:coinId/price");
-    const chartMatch = useMatch("/:coinId/chart");
-    const {isLoading: infoLoading, data: infoData} = useQuery<InfoData>(["info" ,coinId], () => fetchCoinInfo(coinId!))
-    const {isLoading: tickersLoading, data: tickersData} = useQuery<PriceData>(["tickers" ,coinId], () => fetchCoinTickers(coinId!), {refetchInterval: 5000})
+    const { coinId } = useParams();
+    const { state } = useLocation() as RouteState;
+    const priceMatch = useMatch('/:coinId/price');
+    const chartMatch = useMatch('/:coinId/chart');
+    const { isLoading: infoLoading, data: infoData } = useQuery<InfoData>(['info', coinId], () => fetchCoinInfo(coinId!));
+    const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(['tickers', coinId], () => fetchCoinTickers(coinId!), { refetchInterval: 5000 });
     const loading = infoLoading && tickersLoading;
     // const [loading, setLoading] = useState(true);
     // const [info, setInfo] = useState<InfoData>();
@@ -166,66 +166,57 @@ function Coin() {
     return (
         <Container>
             <Helmet>
-                <title>
-                    {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
-                </title>
+                <title>{state?.name ? state.name : loading ? 'Loading...' : infoData?.name}</title>
             </Helmet>
             <Header>
-                <Title>{state?.name ? state.name : loading ? "Loading..." : infoData?.name}</Title>
+                <Title>{state?.name ? state.name : loading ? 'Loading...' : infoData?.name}</Title>
                 <BackPageBtn>
-                    <Link to={'/'}>
-                        {!loading && <>&larr;</> }
-                    </Link>
+                    <Link to={'/'}>{!loading && <>&larr;</>}</Link>
                 </BackPageBtn>
             </Header>
-                {loading ?
-                    (
-                        <Loader>
-                            Loading...
-                        </Loader>
-                    ) :
-                    (
-                        <>
-                            <Overview>
-                                <OverviewItem>
-                                    <span>Rank:</span>
-                                    <span>{infoData?.rank}</span>
-                                </OverviewItem>
-                                <OverviewItem>
-                                    <span>Symbol:</span>
-                                    <span>${infoData?.symbol}</span>
-                                </OverviewItem>
-                                <OverviewItem>
-                                    <span>Price:</span>
-                                    <span>${tickersData?.quotes.USD.price.toFixed(3)}</span>
-                                </OverviewItem>
-                            </Overview>
-                            <Description>{infoData?.description}</Description>
-                            <Overview>
-                                <OverviewItem>
-                                    <span>Total Suply:</span>
-                                    <span>{tickersData?.total_supply}</span>
-                                </OverviewItem>
-                                <OverviewItem>
-                                    <span>Max Supply:</span>
-                                    <span>{tickersData?.max_supply}</span>
-                                </OverviewItem>
-                            </Overview>
+            {loading ? (
+                <Loader>Loading...</Loader>
+            ) : (
+                <>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Rank:</span>
+                            <span>{infoData?.rank}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Symbol:</span>
+                            <span>${infoData?.symbol}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Price:</span>
+                            <span>${tickersData?.quotes.USD.price.toFixed(3)}</span>
+                        </OverviewItem>
+                    </Overview>
+                    <Description>{infoData?.description}</Description>
+                    <Overview>
+                        <OverviewItem>
+                            <span>Total Suply:</span>
+                            <span>{tickersData?.total_supply}</span>
+                        </OverviewItem>
+                        <OverviewItem>
+                            <span>Max Supply:</span>
+                            <span>{tickersData?.max_supply}</span>
+                        </OverviewItem>
+                    </Overview>
 
-                            <Tabs>
-                                <Tab isActive={chartMatch !== null}>
-                                    <Link to={`/${coinId}/chart`}>Chart</Link>
-                                </Tab>
-                                <Tab isActive={priceMatch !== null}>
-                                    <Link to={`/${coinId}/price`}>Price</Link>
-                                </Tab>
-                            </Tabs>
-                            <Outlet context={coinId}/>
-                        </>
-                    )}
+                    <Tabs>
+                        <Tab isActive={chartMatch !== null}>
+                            <Link to={`/${coinId}/chart`}>Chart</Link>
+                        </Tab>
+                        <Tab isActive={priceMatch !== null}>
+                            <Link to={`/${coinId}/price`}>Price</Link>
+                        </Tab>
+                    </Tabs>
+                    <Outlet context={{ coinId, tickersData }} />
+                </>
+            )}
         </Container>
     );
 }
-
 
 export default Coin;
--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,5 +1,40 @@
+import { useOutletContext } from 'react-router-dom';
+import styled from 'styled-components';
+import { PriceData } from './Coin';
+
+const List = styled.div``;
+
+const ListTtl = styled.b``;
+
+const ListValue = styled.p``;
+
+interface PriceProps {
+    tickersData: PriceData;
+}
+
+interface UnitProps {
+    data: number;
+}
+
+const UnitData = styled(unit)``;
+
+function unit({ data }: UnitProps) {
+    return <ListValue>$ {data.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</ListValue>;
+}
+
 function Price() {
-    return <h1>Price</h1>;
+    const { tickersData } = useOutletContext<PriceProps>();
+    const quotes = tickersData.quotes.USD;
+
+    return (
+        <>
+            <List>
+                <ListTtl>시총</ListTtl>
+                <ListValue>$ {quotes.market_cap.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</ListValue>
+                <UnitData data={quotes.market_cap}></UnitData>
+            </List>
+        </>
+    );
 }
 
 export default Price;
